7899786b05a068106f1d65da955a23d422aeba0d
mittnavnermike <niiimon@gmail.com>
2020-09-24 19:32:11 +0200

    updated global types

--- web/types/index.ts
+++ web/types/index.ts
@@ -1,6 +1,5 @@
-import { CSSProp } from 'styled-components'
-// https://styled-components.com/docs/api#usage-with-typescript
-import {} from 'styled-components/cssprop'
+// Helpers
+// https://github.com/microsoft/TypeScript/pull/40336
 
 export type FlexBoxAlignItems =
   | 'flex-start'
@@ -16,42 +15,6 @@ export type FlexBoxJustifyContent =
   | 'space-between'
   | 'space-around'
 
-export type SpacingMixins =
-  | 'm'
-  | 'ml'
-  | 'mt'
-  | 'mr'
-  | 'mb'
-  | 'my'
-  | 'mx'
-  | 'p'
-  | 'pl'
-  | 'pt'
-  | 'pr'
-  | 'pb'
-  | 'py'
-  | 'px'
-  | 'gap'
-  | string
-
-export type ResponsiveColumns = {
-  xs?: number
-  sm?: number
-  md?: number
-  lg?: number
-  xl?: number
-  xxl?: number
-}
-
-export type ResponsiveReverse = {
-  xs?: boolean
-  sm?: boolean
-  md?: boolean
-  lg?: boolean
-  xl?: boolean
-  xxl?: boolean
-}
-
 export type BorderProps =
   | 'border'
   | 'border-top'
@@ -59,35 +22,25 @@ export type BorderProps =
   | 'border-bottom'
   | 'border-left'
 
-export type SpacingUnits =
-  | 'xs'
-  | 'sm'
-  | 'md'
-  | 'lg'
-  | 'xl'
-  | 'section'
-  | 'gutter'
-
-export type BreakPoints = {
-  xs?: number
-  sm?: number
-  md?: number
-  lg?: number
-  xl?: number
-  xxl?: number
-}
-
-export type FontDeclarationObject = {
-  [key: string]:
-    | string
-    | {
-        size: string
-        css?: CSSProp
-      }
-}
-
-export type FontDeclaration = string | FontDeclarationObject
-
-export type responsiveFontDeclaration = {
-  [key: string]: FontDeclaration
-}
+export type NormalizedNumber =
+  | 0
+  | 0.05
+  | 0.1
+  | 0.15
+  | 0.2
+  | 0.25
+  | 0.3
+  | 0.35
+  | 0.4
+  | 0.45
+  | 0.5
+  | 0.55
+  | 0.6
+  | 0.65
+  | 0.7
+  | 0.75
+  | 0.8
+  | 0.85
+  | 0.9
+  | 0.95
+  | 1
