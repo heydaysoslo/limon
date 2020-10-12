719cada26512035862a80614677fc2cf545a25ec
mittnavnermike <niiimon@gmail.com>
2020-10-12 16:50:18 +0200

    :books: documented checkIfStoreOpen

--- web/utils/openingHours.js
+++ web/utils/openingHours.js
@@ -12,23 +12,29 @@ const WEEKDAYS = [
 ]
 
 const checkIfStoreIsOpen = openingHours => {
-  // console.log('openingHours', openingHours)
   if (!openingHours) return null
   const now = utcToZonedTime(new Date(), 'Europe/Oslo')
   const day = now.getDay()
+  // Convert day from number into string eg. 0 === 'sunday' || 1 === 'monday'
   const weekday = WEEKDAYS[day]
+  // Find opening hours from sanity
   const openingDay = openingHours[weekday]
+  // Extract input to use in date constructors eg. openingTime and closingTime
   const month = now.getMonth()
   const year = now.getFullYear()
   const date = now.getDate()
+  // Extract opening and closing times from sanity
   let [openingHour, openingMinute] = openingDay.from
     .split(':')
     .map(item => parseInt(item))
   let [closingHour, closingMinute] = openingDay.to
     .split(':')
     .map(item => parseInt(item))
+  // Construct opening date with opening time
   const openingTime = new Date(year, month, date, openingHour, openingMinute, 0)
+  // Construct closing date with closing time
   const closingTime = new Date(year, month, date, closingHour, closingMinute, 0)
+  // Check if now is between opening and closing time
   return isWithinInterval(now, {
     start: openingTime,
     end: closingTime
