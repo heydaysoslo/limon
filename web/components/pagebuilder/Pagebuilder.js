9f02cc2f9ed02c0a146b622b8428d1625bf3dbfd
mittnavnermike <niiimon@gmail.com>
2020-11-19 00:59:57 +0100

    ⚡️ Dynamic pagebuilder

--- web/components/pagebuilder/Pagebuilder.js
+++ web/components/pagebuilder/Pagebuilder.js
@@ -1,29 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
-import styled, { css, ThemeProvider } from 'styled-components'
+import styled, { css } from 'styled-components'
 
-import CardSection from './CardSection'
-import TextSection from './TextSection'
-import FullImageSection from './FullImageSection'
-import Section from './Section'
-import TextImageSplit from './TextImageSplit'
-import VideoSection from './VideoSection'
-import Tabs from '../elements/Tabs'
 import Stagger from '@heydays/animation/Stagger'
-import Menu from '../Menu'
 import ThemeResolver from '@heydays/ThemeResolver'
 
 const sectionTypes = {
-  section: 'pagebuilder/Section.js',
-  cardSection: 'pagebuilder/CardSection.js',
-  textSection: 'pagebuilder/TextSection.js',
-  imageSection: 'pagebuilder/FullImageSection.js',
-  textImageSplit: 'pagebuilder/TextImageSplit.js',
-  carousel: 'pagebuilder/TextImageSplit.js',
-  tabs: 'elements/Tabs.js',
-  videoSection: 'pagebuilder/VideoSection',
-  menu: 'Menu.js',
-  scrollAnchor: 'pagebuilder/ScrollAnchor'
+  section: dynamic(() => import('pagebuilder/Section.js')),
+  cardSection: dynamic(() => import('pagebuilder/CardSection.js')),
+  textSection: dynamic(() => import('pagebuilder/TextSection.js')),
+  imageSection: dynamic(() => import('pagebuilder/FullImageSection.js')),
+  textImageSplit: dynamic(() => import('pagebuilder/TextImageSplit.js')),
+  carousel: dynamic(() => import('pagebuilder/TextImageSplit.js')),
+  tabs: dynamic(() => import('elements/Tabs.js')),
+  videoSection: dynamic(() => import('pagebuilder/VideoSection')),
+  menu: dynamic(() => import('Menu.js')),
+  scrollAnchor: dynamic(() => import('pagebuilder/ScrollAnchor'))
 }
 
 const StyledPageBuilder = styled.div(
@@ -53,16 +45,9 @@ const PageBuilder = ({ sections }) => {
 }
 
 const PageBuilderResolver = ({ section, ...props }) => {
-  const [Component, setComponent] = useState(null)
-
-  useEffect(() => {
-    if (sectionTypes[section._type]) {
-      // Dynamically import template
-      import(`../../components/${sectionTypes[section._type]}`)
-        .then(comp => setComponent(() => comp.default))
-        .catch(err => console.log(err))
-    }
-  }, [section])
+  const Component = sectionTypes?.[section?._type]
+    ? sectionTypes[section._type]
+    : null
 
   return Component ? (
     <ThemeResolver themeName={section?.themeName}>
